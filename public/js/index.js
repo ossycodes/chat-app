7508ded5d04a85fe30794989ec689ad63c43e478
osaigbovoemmanuel1 <osaigbovoemmanuel1@gmail.com>
2020-05-13 22:51:19 +0100

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('connected to server');
-    socket.emit('createMessage', {
-        from: 'julie@example.com',
-        text: 'Yooo!'
-    });
-
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -17,15 +17,30 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
     console.log("New user connected");
 
-    socket.emit('newMessage', {
-        from: 'mike@example.com',
-        text: 'Hey what is giong on ?',
-        createdAt: 123
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        // socket.emit('newMessage')
+
+        // send to all connected clients
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
+
+        //send back to client
+        // socket.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+        
+        //send to all clients except sender
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+
     });
 
     socket.on('disconnect', () => {
